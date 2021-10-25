9499cd2e0c1bb3182f0e4c5df4b422a512772532
jackshin11 <92985335+jackshin11@users.noreply.github.com>
2021-10-25 21:24:09 +0900

    땡겨 메세지 추가

--- 01_hello_git/src/com/kh/git/HelloGit.java
+++ 01_hello_git/src/com/kh/git/HelloGit.java
@@ -15,6 +15,7 @@ public class HelloGit {
 		System.out.println("333");
 		System.out.println("이제 github repo에서 관리한다!!");
 		System.out.println("원격 저장소에서 작성!!!!");
+		System.out.println("한번에 땡겨주세요!!!");
 	
 		
 	}
