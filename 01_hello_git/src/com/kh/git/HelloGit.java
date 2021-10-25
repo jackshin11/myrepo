87209a51aea6e2090902bbfbdfd89fbe0b17bfb2
jackshin11 <jkshin9000@gmail.com>
2021-10-25 20:03:21 +0900

    1234

--- 01_hello_git/src/com/kh/git/HelloGit.java
+++ 01_hello_git/src/com/kh/git/HelloGit.java
@@ -8,5 +8,14 @@ public class HelloGit {
 		System.out.println("hello animal~");
 
 	}
+	
+	public void merge() {
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		
+		
+	}
 
 }
