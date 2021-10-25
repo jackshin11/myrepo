801da80bc49fee0ee02904d5581c01de832eecb0
jackshin11 <jkshin9000@gmail.com>
2021-10-25 20:07:32 +0900

    2 333333 4

--- 01_hello_git/src/com/kh/git/HelloGit.java
+++ 01_hello_git/src/com/kh/git/HelloGit.java
@@ -10,9 +10,9 @@ public class HelloGit {
 	}
 	
 	public void merge() {
-		System.out.println("1");
+	
 		System.out.println("2");
-		System.out.println("3");
+		System.out.println("333333");
 		System.out.println("4");
 		
 		
